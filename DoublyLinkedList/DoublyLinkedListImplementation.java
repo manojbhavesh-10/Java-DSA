ff896ec2a3a8ebc3b80d95640799df43a8dfcd85
Manoj Bhavesh <119804129+manojbhavesh-10@users.noreply.github.com>
2026-01-16 12:39:02 +0530

    Doubly Linked list Insertiion implementation 16 Jan 2026

--- DoublyLinkedList/DoublyLinkedListImplementation.java
+++ DoublyLinkedList/DoublyLinkedListImplementation.java
@@ -17,7 +17,6 @@ public class DoublyLinkedListImplementation {
         }
         size += 1;
     }
-
     public void printDoublyLinkedList() {
         Node current = head;
         System.out.print("[");
@@ -100,7 +99,6 @@ public class DoublyLinkedListImplementation {
             System.out.println("IndexOutOfBound: Please provide the proper Kth value");
         }
     }
-
     public void deleteByNode(Node node) {
         if(node == null) {
             System.out.println("NullPointerException: Please provide proper node!");
@@ -116,5 +114,83 @@ public class DoublyLinkedListImplementation {
             backNode.back = prevNode;
             node.next = node.back = null;
         }
+    }
+
+    // functions - implementing by before the node
+    public void insertHead(int num) {
+        Node node = new Node(num);
+        if(head == null) {
+            head = node;
+        } else {
+            head.back = node;
+            node.next = head;
+            head = node;
+        }
+    }
+
+    public void insertTail(int num) {
+        Node node = new Node(num);
+        Node current = head, prev = null;
+        if(head == null) {
+            head = node;
+            return;
+        } else {
+            while (current.next != null) {
+                current = current.next;
+            }
+            prev = current.back;
+            if (prev != null) {
+                prev.next = node;
+                node.back = prev;
+                node.next = current;
+                current.back = node;
+            } else { // it's a single node
+                current.back = node;
+                node.next = current;
+                head = node;
+            }
+        }
+
+    }
+
+    public void insertByKth(int k, int num) {
+        Node node = new Node(num);
+        if(head == null) {
+            head = node;
+            System.out.println("Doubly linked list is empty!");
+
+            return;
+        } else if(head.next == null) {
+            head.next = node;
+            node.back = head;
+            return;
+        } else {
+            Node current = head, prev = null;
+            int cnt = 0;
+            while(current != null) {
+                cnt += 1;
+                if(cnt == k) {
+                    prev = current.back;
+                    prev.next = node;
+                    node.next = current;
+                    node.back = prev;
+                    current.back = node;
+                    return;
+                }
+                current = current.next;
+            }
+        }
+        System.out.println("IndexOutOfBound: Please provide a proper Kth value!");
+    }
+
+    public void insertByNode(Node current, int num) {
+        Node node = new Node(num);
+        
+        Node prev = current.back;
+        prev.next = node;
+        node.next = current;
+        current.back = node;
+        node.back = prev;
+
     }
 }
--- DoublyLinkedList/Main.java
+++ DoublyLinkedList/Main.java
@@ -26,7 +26,17 @@ public class Main {
 //        list.deleteByK(1);
 //
 //        list.deleteByK(2);
-        list.deleteByNode(list.head.next.next);
+//        list.deleteByNode(list.head.next.next);
+//        list.insertHead(-1);
+//        list.insertHead(-3);
+//        list.insertHead(-7);
+//        list.insertTail(-5);
+//        list.insertTail(-7);
+//        list.insertTail(-3);
+//        list.insertByKth(3, -1);
+//        list.insertByKth(5, -8);
+//        list.insertByKth(10, -4);
+        list.insertByNode(list.head.next.next.next.next.next, -1);
         list.printDoublyLinkedList();
 
 
